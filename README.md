3eaf557ce53acf39d1d7d887d0c03dfc28e823dd
Ethan Tam <ethantam@reelable.social>
2021-02-08 19:33:36 -0500

    Working Ethereum ticker with icon.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # ethereum-ticker
+
 A status bar Ethereum ticker for OS X.
+
+# Development
+
+ethereum-ticker was written using Python 3.9.0. You can run it with:
+
+```
+pip3 install -r requirements.txt
+python3 main.py
+```
---
+++ main.py
@@ -0,0 +1,32 @@
+from collections import OrderedDict
+
+import rumps
+from json import loads
+import requests
+
+class EthereumTicker(rumps.App):
+    ICON = "eth.ico"
+    UPDATE_FREQUENCY = 60
+    
+    #TODO: Use endpoint that offers more currencies
+    DEFAULT_CURRENCY = "USD"
+    ETH_URL = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=ethereum"
+    SYMBOLS = OrderedDict([
+        ("USD" , "$")
+    ])
+
+    def __init__(self):
+        super(EthereumTicker, self).__init__(type(self).__name__, icon=EthereumTicker.ICON)
+        self.padded = False
+        self.currency = EthereumTicker.DEFAULT_CURRENCY
+        self.run()
+    
+    @rumps.timer(UPDATE_FREQUENCY)
+    def update_price(self, sender):
+        res = requests.get(EthereumTicker.ETH_URL)
+        curr_price = loads(res.text)[0]['current_price']
+        title = "$" + str(curr_price)
+        self.title = title
+
+if __name__ == '__main__':
+    EthereumTicker()
